1251fa48ff983451bf249ad61a8e19fbb0e7238d
TomarAbhin <tomar.abhinav3107@gmail.com>
2026-04-14 12:11:35 +0530

    [Abhinav Tomar] Refactored the code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -109,9 +109,9 @@ const Login = () => {
           <span style={{ fontSize: '0.85rem' }}>OR</span>
         </div>
 
-        <a 
-          href="http://localhost:8080/oauth2/authorization/google" 
-          className="auth-btn secondary" 
+        <a
+          href={`${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`}
+          className="auth-btn secondary"
           style={{ textDecoration: 'none', display: 'flex', marginBottom: '1rem' }}
         >
           <FcGoogle size={20} />
